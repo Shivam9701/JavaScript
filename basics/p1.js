b7ab3186267fbc3018f11c1edd628ee98b79003d
Shivam9701 <shivamkichitthi@gmail.com>
2021-12-05 13:25:20 +0530

    first commit

---
+++ basics/p1.js
@@ -0,0 +1,20 @@
+ceo ={
+    "name" : "dog",
+    'description' : "dw"
+};
+function change(obj){
+    obj.description="ddod";
+};
+change(ceo);
+
+
+var arr1= [];
+arr1[0]="This is m ff";
+arr1.name ="sjks";
+
+len = arr1.length;
+
+for (var i=0; i<len; i++){
+    console.log(arr1[i],len);
+}
+console.log(arr1);
